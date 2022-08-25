ffa09485f2b868f549d2bb40f8e7233400c88600
정형진 <88423849+KeMezz@users.noreply.github.com>
2022-08-25 11:12:17 +0900

    do basic stuffs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,69 @@
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { useState } from "react";
+import { tableData } from "./tableData";
+import "./App.css";
+
 function App() {
-  return <div className="App">Hello, React!</div>;
+  const [data] = useState([...tableData]);
+
+  const columnHelper = createColumnHelper();
+  const columns = [
+    columnHelper.accessor("name", { header: "이름", size: 60 }),
+    columnHelper.accessor("phone", {
+      header: "휴대폰",
+      size: 300,
+      cell: ({ renderValue }) =>
+        renderValue().replace(/(\d{3})(\d{3,4})(\d{4})/, "$1-$2-$3"),
+    }),
+    columnHelper.accessor("birth", { header: "생년월일", size: 80 }),
+    columnHelper.accessor("register_date", { header: "등록일", size: 120 }),
+    columnHelper.accessor("last_edit_date", {
+      header: "최종수정일",
+      size: 120,
+    }),
+  ];
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+  return (
+    <table>
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th key={header.id} style={{ width: header.getSize() }}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
 export default App;
